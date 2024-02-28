bf30af2e13825dfb0e61f35852e3bacde43b4560
jroycode <102575230+jroycode@users.noreply.github.com>
2024-02-28 13:28:35 -0500

    Fixed more image loading

--- loader.js
+++
@@ -1,6 +0,0 @@
-'use client'
-
-export default function myImageLoader({ src, width, quality}) {
-    if(src.startsWith('https://images.pexels.com')) return src
-        return 'https://personal-portfolio-eta-one.vercel.app/${public}'
-}
