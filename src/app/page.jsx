0fbd7e593d862ffbecc1739a3c922d74fe625706
jroycode <102575230+jroycode@users.noreply.github.com>
2024-02-28 12:51:38 -0500

    Initial Changes

---
+++ src/app/page.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+const Homepage = () => {
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* IMAGE CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+          <Image src="/hero.png" alt="" fill className="object-contain" />
+        </div>
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+          {/* TITLE */}
+          <h1 className="text-4xl md:text-6xl font-bold">
+            Designing and Implementing websites of tomorrow.
+          </h1>
+          {/* DESC */}
+          <p className="md:text-xl">
+          From mastering diverse challenges at Trader Joe's to swiftly navigating
+          the complexities of a coding bootcamp, and now thriving as a web developer, my
+          journey reflects an unwavering commitment to rapid learning and a passion for excellence in front-end development
+          </p>
+          {/* BUTTONS */}
+          <div className="w-full flex gap-4">
+            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+              Download CV
+            </button>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Homepage;
